b9795a08665497920457bd8d3321357609e77807
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2015-06-01 13:30:02 -0400

    Complete business logic
    I complete the business logic sections from lectures one and two
    of the iOS development series from Stanford on iTunes. Next, I
    will focus on the layout.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,25 +1,72 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Cyrus Vandrevala on 5/26/15.
-//  Copyright (c) 2015 Cyrus Vandrevala. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTypingANumber = false
+    var operandStack: Array<Double> = Array<Double>()
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else  {
+            userIsInTheMiddleOfTypingANumber = true
+            display.text = digit
+        }
+    }
+    
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "✕": performOperation { $0 * $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        default: break
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
     }
 
-
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+        
+    }
+    
+    
+    
+    
+    
 }
 

3c59348e51704364c6d7982f6391577d8500ea64
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2015-06-05 13:50:54 -0500

    Add basic trig functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -35,6 +35,9 @@ class ViewController: UIViewController {
         case "✕": performOperation { $0 * $1 }
         case "−": performOperation { $1 - $0 }
         case "√": performOperation { sqrt($0) }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "tan": performOperation { tan($0) }
         default: break
         }
     }
